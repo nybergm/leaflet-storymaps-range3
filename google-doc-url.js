8e9e340ffb57c887ed18aa3b1f3245142d3b049b
nybergm <49000679+nybergm@users.noreply.github.com>
2019-11-03 14:39:42 -0500

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1AO6XHL_0JafWZF4KEejkdDNqfuZWUk3SlNlQ6MjlRFM/';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1aj2LTrP3dISTjXMpdqEfq-cPRrwfdvLiWkuRke68aKE/edit#gid=0';
